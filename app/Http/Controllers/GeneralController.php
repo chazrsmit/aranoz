10ffbbde2a8903ab43e51f4b99f90d5b6aadc941
Charlotte <smit-charlotte@outlook.com>
2025-09-26 15:35:29 +0200

    logique homepage

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BlogCategory;
+use App\Models\Product;
 use App\Models\ProductCategory;
 use App\Models\Tag;
 use Illuminate\Console\Application;
@@ -13,11 +14,27 @@ use Inertia\Inertia;
 class GeneralController extends Controller
 {
     public function index() {
+        
+        // products featured in the carousel in the hero
+        // random products ET products where isPinned == 1
+        $prod_pinned = Product::where('isPinned', 1)->get();
+        $prod_random = Product::where('isPinned', 0)->inRandomOrder()->limit(4)->get();
+        $prod_car = $prod_pinned->merge($prod_random);
+        $prod_discount = Product::where('promotion_id', 2)->inRandomOrder()->limit(1)->get();
+        $prod_best = Product::inRandomOrder()->limit(4)->get();
+
+        // ALL products selected randomly for the awesome section
+        $prod_awe = Product::inRandomOrder()->limit(8)->get();
+
         return Inertia::render('Home', [
         // 'canLogin' => Route::has('login'),
         // 'canRegister' => Route::has('register'),
         // 'laravelVersion' => Application::VERSION,
         // 'phpVersion' => PHP_VERSION
+            'prod_car' => $prod_car,
+            'prod_awe' => $prod_awe,
+            'prod_discount' => $prod_discount,
+            'prod_best' => $prod_best
         ]);
     }
 
@@ -36,7 +53,7 @@ class GeneralController extends Controller
         return Inertia::render('Back/Categories', [
             'blog_cats' => $blog_cats,
             'prod_cats' => $prod_cats,
-            'tags' => $tags
+            'tags' => $tags,
         ]);
     }
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import NavFront from '../Components/NavFront.jsx';
 import { Head, Link } from '@inertiajs/react';
 
-export default function Home({ auth }) {
+export default function Home({ auth, prod_car, prod_awe, prod_discount, prod_best }) {
 
     return(
         <>
@@ -9,7 +9,56 @@ export default function Home({ auth }) {
 
             <NavFront auth={auth} />
 
-            <h1>Homepage</h1>
+            {/* Hero with caorusel featuring products */}
+            {prod_car.map(p => (
+                <div key={p.id}></div>
+            ))}
+
+            {/* Categories */}
+                {/* Category fauteuils -> productcategory_id = 7 */}
+                {/* image to use : '/storage/featured/offer_img.png' */}
+
+                {/* Category Canapés -> productcategory_id = 6 */}
+                {/* image to use : '/storage/featured/feature_4.png' */}
+
+                {/* Category Chaises -> productcategory_id = 1 */}
+                {/* image to use : '/storage/featured/feature_2.png' */}
+
+                {/* Category fauteuils -> productcategory_id = 7 */}
+                {/* image to use : '/storage/featured/feature_3.png' */}
+
+
+            {/* Awesome section  : Displays 2 pages of 8 random products (carousel-like navigation). Clicking a product redirects the user to the product detail page.*/}
+
+            {
+                prod_awe.map(p => (
+                    <div key={p.id}></div>
+                ))
+            }
+
+            {/* Weekly sale section:
+            - Displays a 5-day countdown timer that resets to 5 days automatically when it reaches zero.
+            - Highlights one discounted product at random (works only with products already on discount) > so products that had a promotion
+            - The title reflects the discount, '<Weekly Sale 4 Up to 20%'> the displayed product already has that discount applied
+            -Includes an email input to send a 10% coupon to the submitted address (always sends a simple coupon email when a valid email is provided).*/}
+            <img src={prod_discount.image} alt="" />
+            <p>{prod_discount.product}</p>
+            {/* <form onSubmit={onSubmit}>
+                <input type="email_promo" />
+                <button type="submit"></button>
+            </form> */}
+
+            {/* Best seller section : 4 random produits */}
+            {prod_best.map(p => (
+
+                <div key={p.key}></div>
+
+            ))}
+
+            {/* Newsletter section - an input field where the person can enter their email adress > the logic will be added later */}
+
+            
+
         </>
     )
 }
