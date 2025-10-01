ba98e9e0d52c44498d1ea076d0359bb7b191a1fd
Charlotte <smit-charlotte@outlook.com>
2025-10-01 13:51:05 +0200

    start cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function add_to_cart(Request $request){
+
+        $cart = Cart::with('items', 'items.product');
+
+        $cart->create([
+            'user_id' => auth()->user(),
+            'quantity' => $request->quantity,
+            'price' => $request->price,
+        ]);
+    }
 }
